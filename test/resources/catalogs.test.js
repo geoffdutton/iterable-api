012e73b0f6a7de18ad7802b6e12658f2c2aeace2
julianmclain <julianrmclain@gmail.com>
2020-01-13 09:05:52 -1000

    Support Catalog items endpoints and update tests

--- test/resources/catalogs.test.js
+++ test/resources/catalogs.test.js
@@ -1,18 +1,20 @@
 const factory = require('../../lib/resources/catalogs')
 
 const BASE = '/catalogs'
+const FIELD_MAPPINGS_BASE = '/catalogs/{catalogName}/fieldMappings'
+const ITEMS_BASE = '/catalogs/{catalogName}/items'
+
+const request = {
+  get: jest.fn(),
+  post: jest.fn(),
+  put: jest.fn(),
+  patch: jest.fn(),
+  delete: jest.fn()
+}
+const client = factory(request)
 const catalogName = 'fancy-restaurants'
 
 describe(BASE, () => {
-  const request = {
-    get: jest.fn(),
-    post: jest.fn(),
-    put: jest.fn(),
-    delete: jest.fn()
-  }
-
-  const client = factory(request)
-
   it(`GET ${BASE}`, () => {
     client.get({ page: 1, pageSize: 5 })
     expect(request.get).toHaveBeenCalledWith(BASE, { page: 1, pageSize: 5 })
@@ -27,13 +29,15 @@ describe(BASE, () => {
     client.delete({ catalogName })
     expect(request.delete).toHaveBeenCalledWith(`${BASE}/${catalogName}`)
   })
+})
 
-  it(`GET ${BASE}/{catalogName}/fieldMappings`, () => {
+describe(FIELD_MAPPINGS_BASE, () => {
+  it(`GET ${FIELD_MAPPINGS_BASE}`, () => {
     client.fieldMappings.get({ catalogName })
     expect(request.get).toHaveBeenCalledWith(`${BASE}/${catalogName}/fieldMappings`)
   })
 
-  it(`PUT ${BASE}/{catalogName}/fieldMappings`, () => {
+  it(`PUT ${FIELD_MAPPINGS_BASE}`, () => {
     const mappings = {
       mappingsUpdates: [
         {
@@ -44,5 +48,72 @@ describe(BASE, () => {
     }
     client.fieldMappings.set({ catalogName, mappings })
     expect(request.put).toHaveBeenCalledWith(`${BASE}/${catalogName}/fieldMappings`, mappings)
+  })
+})
+
+describe(ITEMS_BASE, () => {
+  it(`GET ${ITEMS_BASE}`, () => {
+    const query = {
+      page: 1,
+      pageSize: 10,
+      orderBy: 'name',
+      sortAscending: false
+    }
+    client.items.get({ catalogName, ...query })
+    expect(request.get).toHaveBeenCalledWith(`${BASE}/${catalogName}/items`, query)
+  })
+
+  it(`GET ${ITEMS_BASE}/{itemId}`, () => {
+    const itemId = '1'
+    client.items.getById({ catalogName, itemId })
+    expect(request.get).toHaveBeenCalledWith(`${BASE}/${catalogName}/items/${itemId}`)
+  })
+
+  it(`PUT ${ITEMS_BASE}`, () => {
+    const itemId = '1'
+    const value = {
+      value: { name: 'The Duke', isVegetarianFriendly: false }
+    }
+    client.items.createOrReplace({ catalogName, itemId, value })
+    expect(request.put).toHaveBeenCalledWith(`${BASE}/${catalogName}/items/${itemId}`, value)
+  })
+
+  it(`PATCH ${ITEMS_BASE}`, () => {
+    const itemId = '1'
+    const update = {
+      update: { recentlyUpdated: true }
+    }
+    client.items.createOrUpdate({ catalogName, itemId, update })
+    expect(request.patch).toHaveBeenCalledWith(`${BASE}/${catalogName}/items/${itemId}`, update)
+  })
+
+  it(`POST ${ITEMS_BASE}`, () => {
+    const data = {
+      documents: {
+        1: {
+          name: 'The Duke',
+          isVegetarianFriendly: false
+        },
+        2: {
+          name: 'Le Chateau Rockinbleau',
+          isVegetarianFriendly: true
+        }
+      },
+      replaceUploadedFieldsOnly: true
+    }
+    client.items.bulkCreate({ catalogName, data })
+    expect(request.post).toHaveBeenCalledWith(`${BASE}/${catalogName}/items`, data)
+  })
+
+  it(`DELETE ${ITEMS_BASE}`, () => {
+    const itemIds = ['1', '2', '3']
+    client.items.delete({ catalogName, itemIds })
+    expect(request.delete).toHaveBeenCalledWith(`${BASE}/${catalogName}/items`, { itemIds })
+  })
+
+  it(`DELETE ${ITEMS_BASE}/{itemId}`, () => {
+    const itemId = '1'
+    client.items.deleteById({ catalogName, itemId })
+    expect(request.delete).toHaveBeenCalledWith(`${BASE}/${catalogName}/items/${itemId}`)
   })
 })
